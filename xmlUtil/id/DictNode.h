7ea3873a34604729ada4b58261c31490fca9c7d4
jrb <>
2001-05-17 21:09:18 +0000

    Id Dictionary headers: somewhat closer to reality

--- xmlUtil/id/DictNode.h
+++ xmlUtil/id/DictNode.h
@@ -1,4 +1,4 @@
-// $Header$
+// $Header: /nfs/slac/g/glast/ground/cvs/xmlUtil/xmlUtil/id/DictNode.h,v 1.1 2001/05/09 23:52:43 jrb Exp $
 #ifndef XMLUTIL_DICTNODE_H
 #define XMLUTIL_DICTNODE_H
 
@@ -7,7 +7,9 @@
 #include "xmlUtil/id/DictConstraints.h"
 
 #include <vector>
-
+#include <algorithm>
+#include <set>
+#include <iterator>
 
 namespace xmlUtil {
   //! DictNode represents a node in an identifier dictionary.
@@ -15,36 +17,67 @@ namespace xmlUtil {
       for that to be very attractive.  Instead, make virtually everything
       private and let IdDict be a friend.
   */
+  class DictFieldMan;
+
   class DictNode {
   public:
     //! Construct a node from its XML representation (probably
     //! recursively construct children as well)
-    DictNode(DOM_Element elt);
-    ~DictNode();  // similarly, need to delete all children recursively
+    DictNode(DOM_Element elt, DictNode* parent, DictFieldMan *fieldMan); 
+    /* IN PROGRESS */
+    ~DictNode();  // similarly, delete all children recursively /* TO DO */
+    const DictField& getField() const {return *m_field;};
+    const DictConstraints& getValueConstraints() const { 
+      return *m_myConstraints;};
+    const DictConstraints& getParentConstraints() const {
+      return *m_parConstraints;};
 
   private:
     friend class IdDict;
     friend class IdConverter;   //maybe
+    friend struct POrder;
 
+    // Might want to use some other form of collection for 
+    // child DictNodes
+    typedef std::vector<DictNode*> Nodes;
+    typedef std::vector<DictNode*>::iterator NodeIterator;
+
+    // Nested class allowing sorting of child nodes by m_minValue of
+    // parent constraint
+    struct POrder {
+      bool operator()(DictNode* a, DictNode* b) {
+        return ((a->getParentConstraints()).getMin() <
+                (b->getParentConstraints()).getMin()   ); }
+    }; 
+
+    const Nodes&     getChildren() const {return m_children;}
     // More constructor, for when dictionary is built dynamically
     // rather than from xml file
-    DictNode(DictField& field);
-    DictNode(DictField& field, DictConstraints& parConstraints);
-    DictNode(DictField& field, DictConstraints& parConstraints,
-             DictConstraints& myConstraints);
-    const DictField& getField() const {return *field;};
-    const Nodes&     getChildren() const {return children;};
-    const DictConstraints& getValueConstraints() const { 
-      return *myConstraints;};
-    const DictConstraints& getParentConstraints() const {
-      return *parConstraints;};
-    bool  allowed(const unsigned value);
+    /*    DictNode(DictField& field);                             TO DO
+    DictNode(DictField& field, DictConstraints& parConstraints);  TO DO
+    DictNode(DictField& field, DictConstraints& parConstraints,   TO DO
+             DictConstraints& myConstraints);                     TO DO */
+    bool  allowed(const unsigned value) const;            
     bool  allowedChild(std::string childField, unsigned childValue, 
-                       unsigned value);
-    bool  allowedChild(unsigned childValue, unsigned value);
-    bool  addChild(DictNode* child);
+                       unsigned value) const {return true;}         /* TO DO */
+    bool  allowedChild(unsigned childValue, unsigned value) const {
+      return true;  }  /* TO DO */
+    bool  addChild(DictNode* child)    {return true;}            /* TO DO */
 
-    typedef std::vector<DictNode*> Nodes;
+
+    //! Check that collection of children define appropriately disjoint
+    //! collections of Identifiers.  In the process may do some sorting
+    //! of child nodes, so these functions are not const.  
+    bool  consistent();                        /* done, sort of */
+
+    //! Insert allowed values for this DictNode into a set so we
+    //! can use set operations to look for overlap
+    void insertValues(std::set<unsigned>& aSet);
+
+    //! Check that for nodes in the interval demarcated by [first, last]
+    //! value constraints give pairwise-disjoint sets
+    bool  valuesDisjoint(NodeIterator start, NodeIterator last);  /* TO DO */
+
 
     DictNode() {};                    /*< don't allow uninitialized node */
     Nodes            m_children;      /*< collection of child nodes */
