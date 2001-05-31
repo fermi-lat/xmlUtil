77f912f4ca5860d018e4e2d1bfd1c67538c13d53
jrb <>
2001-05-31 22:56:40 +0000

    Progress on id dictionary classes; not yet done with DictNode, IdDict

--- xmlUtil/id/IdDict.h
+++ xmlUtil/id/IdDict.h
@@ -1,4 +1,4 @@
-// $Header: /nfs/slac/g/glast/ground/cvs/xmlUtil/xmlUtil/id/IdDict.h,v 1.1 2001/05/09 23:52:43 jrb Exp $
+// $Header: /nfs/slac/g/glast/ground/cvs/xmlUtil/xmlUtil/id/IdDict.h,v 1.2 2001/05/17 21:09:17 jrb Exp $
 #ifndef XMLUTIL_IDDICT_H
 #define XMLUTIL_IDDICT_H
 
@@ -11,6 +11,20 @@
 
 namespace xmlUtil {
   //! IdDict corresponds to the xml element idDict in gdd.dtd
+  /*! The structure of an IdDict is
+       a collection of DictFields, each of which may point to an associated
+       DictConstraints object.  The fields may be accessed via the single
+       DictFieldMan object associated with the dictionary
+       A collection of nodes arranged hierarchically.  Each node
+          MUST reference its parent node (unless it's the root
+            of the hierarchy, in which case it has none)
+          MUST reference a DictField object in the collection
+          MAY reference an additional DictConstraints object which
+            further restricts the values its associated field may
+            take on
+          MAY referemce yet another DictConstraints object which
+            restricts the values its parent node field may take on
+  */
   class DictNode;
   class DictField;
   class DictFieldMan;
