143e4ad695c844525ec6e1d8a7c46c5f0fdb0856
jrb <>
2001-12-13 01:07:45 +0000

    IdKey object can be built from vector or deque

--- xmlUtil/id/IdKey.h
+++ xmlUtil/id/IdKey.h
@@ -1,39 +1,53 @@
-// $Header:  $
+// $Header: /nfs/slac/g/glast/ground/cvs/xmlUtil/xmlUtil/id/IdKey.h,v 1.1 2001/12/08 00:15:59 jrb Exp $
 #ifndef XMLUTIL_IDKEY_H
 #define XMLUTIL_IDKEY_H
 
 #include <vector>
+#include <deque>
 #include <string>
+#include <iterator>
 
-#include "xmlUtil/id/NamedId.h"
 
 namespace xmlUtil {
 
-  //*  IdKey is a compressed, sortable version of an identifier which may
-  //*  be used as a key for a map.
+  ///  IdKey is a compressed, sortable version of an identifier which may
+  ///  be used as a key for a map.
   class IdKey {
   public:
-    //* Construct an IdKey from a "standard" identifier
-    IdKey(const Identifier& id);
+    /// Construct an IdKey from  a vector
+    IdKey(const std::vector<unsigned int> id);
+
+    /// Construct an IdKey from  a deque
+    IdKey(const std::deque<unsigned int> id);
+
+    IdKey() {}
 
     /*
-    // Construct an IdKey from an identifier whose fields are expressed
-    // as strings         NYI
-    //    IdKey(const std::vector<std::string>&);  
+       Construct an IdKey from an identifier whose fields are expressed
+       as strings         NYI
+          IdKey(const std::vector<std::string>&);  
+       Do we also want to go the other direction? (IdKey to std::string)
    */
+
     ~IdKey();
 
-    //* The comparison function needed when making a map
+    /// The comparison function needed when making a map
     class ltkey { 
     public:
       bool operator()(IdKey key1, IdKey key2) const;
     };
+    friend  class ltkey;       
 
   private:
-    typedef std::vector<unsigned int>::const_iterator KeyIt;
+    /** template function which does the actual work of building
+        the new IdKey.  In principle it could be instantiated for
+        any container whose value type is unsigned int. */
+    template<class Container>
+    void fillFrom(const Container& con);
 
-    friend  ltkey;
-    std::vector<unsigned int> m_key;
+    typedef std::vector<unsigned int>::const_iterator KeyIt;
+    /// Compressed identifier is stored in /a m_key
+    std::vector<unsigned int> m_key; 
   };
 }
 
