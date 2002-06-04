515764ce87bd3bcbf3bb572b3092a62b8c3f4ca2
jrb <>
2002-06-04 22:43:59 +0000

    Eliminate a harmless compiler warnings ("unused parameter", known to be unused)

--- xmlUtil/id/IdOpDisappear.h
+++ xmlUtil/id/IdOpDisappear.h
@@ -1,4 +1,4 @@
-// $Header: /nfs/slac/g/glast/ground/cvs/xmlUtil/xmlUtil/id/IdOpDisappear.h,v 1.2 2001/08/24 22:46:38 jrb Exp $
+// $Header: /nfs/slac/g/glast/ground/cvs/xmlUtil/xmlUtil/id/IdOpDisappear.h,v 1.3 2001/09/25 21:43:06 jrb Exp $
 
 #ifndef XMLUTIL_IDOPDISAPPEAR_H
 #define XMLUTIL_IDOPDISAPPEAR_H
@@ -15,7 +15,7 @@ namespace xmlUtil {
   public:
     IdOpDisappear(DOM_Element) {}
     ~IdOpDisappear() {}
-    NamedId * convert(const NamedId& inputId) {return new NamedId();}
+    NamedId * convert(const NamedId& ) {return new NamedId();}
     virtual std::string myOp() const {return std::string("DISAPPEAR ");}
   };
 }    
