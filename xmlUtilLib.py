817843ca14df8b196f76a3e9fb1fdefe75f5b490
jrb <>
2012-08-16 23:50:47 +0000

    for Windows no-installed-headers

--- xmlUtilLib.py
+++ xmlUtilLib.py
@@ -1,7 +1,9 @@
-# $Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/xmlUtil/xmlUtilLib.py,v 1.1 2008/07/09 21:13:48 glastrm Exp $
+# $Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/xmlUtil/xmlUtilLib.py,v 1.2 2009/08/06 23:59:48 jrb Exp $
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['xmlUtil'])
+	if env['PLATFORM'] == 'win32':
+	    env.Tool('findPkgPath', package = 'xmlUtil') 
     env.Tool('xmlBaseLib')
     env.Tool('facilitiesLib')
     env.Tool('addLibrary', library = env['xercesLibs'])
